664552d99aef5f27f85520b7d6de58db3e1fdf28
Mateo Lohezic <mateolohezic@gmail.com>
2023-12-15 01:35:44 -0300

    New: Cards pacientes

--- src/Pages/Pacientes/Pacientes.jsx
+++ src/Pages/Pacientes/Pacientes.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './pacientes.css'
 import BarraDeNavegacion from '../../Components/BarraDeNavegacion/BarraDeNavegacion'
 import addIcon from '../../assets/addIcon.png'
+import pacienteDefecto from '../../assets/pacienteDefecto.jpeg'
+import turnoIcon from '../../assets/turnoIcon.png'
 import BuscadorPacientes from '../../Components/BuscadorPacientes/BuscadorPacientes'
 
 function Pacientes() {
@@ -29,6 +31,215 @@ function Pacientes() {
                     </a>
                 </section>
                 <BuscadorPacientes onSubmit={onSearch}/>
+                <section className='contenedorCardsPacientes'>
+                    <article className='cardPaciente'>
+                        <div className='imagenCardPaciente'>
+                            <img src={pacienteDefecto} alt="Paciente" />
+                        </div>
+                        <div className='detallesCardPaciente'>
+                            <div className='tituloCardPaciente'>
+                                <h2>Mateo Bernabé Lohezic</h2>
+                                <h3>Peso actual: 300 kg</h3>
+                            </div>
+                            <div className='turnoCardPaciente'>
+                                <img src={turnoIcon} alt="Proximo turno" />
+                                <div>
+                                    <h2>Proximo turno</ h2>
+                                    <h3>Martes 13 de agoto</h3>
+                                </div>
+                            </div>
+                            <div className='verCardPaciente'>
+                                <a href="/">
+                                    <button type="button">Ver planilla</button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                    <article className='cardPaciente'>
+                        <div className='imagenCardPaciente'>
+                            <img src={pacienteDefecto} alt="Paciente" />
+                        </div>
+                        <div className='detallesCardPaciente'>
+                            <div className='tituloCardPaciente'>
+                                <h2>Mateo Bernabé Lohezic</h2>
+                                <h3>Peso actual: 300 kg</h3>
+                            </div>
+                            <div className='turnoCardPaciente'>
+                                <img src={turnoIcon} alt="Proximo turno" />
+                                <div>
+                                    <h2>Proximo turno</ h2>
+                                    <h3>Martes 13 de agoto</h3>
+                                </div>
+                            </div>
+                            <div className='verCardPaciente'>
+                                <a href="/">
+                                    <button type="button">Ver planilla</button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                    <article className='cardPaciente'>
+                        <div className='imagenCardPaciente'>
+                            <img src={pacienteDefecto} alt="Paciente" />
+                        </div>
+                        <div className='detallesCardPaciente'>
+                            <div className='tituloCardPaciente'>
+                                <h2>Mateo Bernabé Lohezic</h2>
+                                <h3>Peso actual: 300 kg</h3>
+                            </div>
+                            <div className='turnoCardPaciente'>
+                                <img src={turnoIcon} alt="Proximo turno" />
+                                <div>
+                                    <h2>Proximo turno</ h2>
+                                    <h3>Martes 13 de agoto</h3>
+                                </div>
+                            </div>
+                            <div className='verCardPaciente'>
+                                <a href="/">
+                                    <button type="button">Ver planilla</button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                    <article className='cardPaciente'>
+                        <div className='imagenCardPaciente'>
+                            <img src={pacienteDefecto} alt="Paciente" />
+                        </div>
+                        <div className='detallesCardPaciente'>
+                            <div className='tituloCardPaciente'>
+                                <h2>Mateo Bernabé Lohezic</h2>
+                                <h3>Peso actual: 300 kg</h3>
+                            </div>
+                            <div className='turnoCardPaciente'>
+                                <img src={turnoIcon} alt="Proximo turno" />
+                                <div>
+                                    <h2>Proximo turno</ h2>
+                                    <h3>Martes 13 de agoto</h3>
+                                </div>
+                            </div>
+                            <div className='verCardPaciente'>
+                                <a href="/">
+                                    <button type="button">Ver planilla</button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                    <article className='cardPaciente'>
+                        <div className='imagenCardPaciente'>
+                            <img src={pacienteDefecto} alt="Paciente" />
+                        </div>
+                        <div className='detallesCardPaciente'>
+                            <div className='tituloCardPaciente'>
+                                <h2>Mateo Bernabé Lohezic</h2>
+                                <h3>Peso actual: 300 kg</h3>
+                            </div>
+                            <div className='turnoCardPaciente'>
+                                <img src={turnoIcon} alt="Proximo turno" />
+                                <div>
+                                    <h2>Proximo turno</ h2>
+                                    <h3>Martes 13 de agoto</h3>
+                                </div>
+                            </div>
+                            <div className='verCardPaciente'>
+                                <a href="/">
+                                    <button type="button">Ver planilla</button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                    <article className='cardPaciente'>
+                        <div className='imagenCardPaciente'>
+                            <img src={pacienteDefecto} alt="Paciente" />
+                        </div>
+                        <div className='detallesCardPaciente'>
+                            <div className='tituloCardPaciente'>
+                                <h2>Mateo Bernabé Lohezic</h2>
+                                <h3>Peso actual: 300 kg</h3>
+                            </div>
+                            <div className='turnoCardPaciente'>
+                                <img src={turnoIcon} alt="Proximo turno" />
+                                <div>
+                                    <h2>Proximo turno</ h2>
+                                    <h3>Martes 13 de agoto</h3>
+                                </div>
+                            </div>
+                            <div className='verCardPaciente'>
+                                <a href="/">
+                                    <button type="button">Ver planilla</button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                    <article className='cardPaciente'>
+                        <div className='imagenCardPaciente'>
+                            <img src={pacienteDefecto} alt="Paciente" />
+                        </div>
+                        <div className='detallesCardPaciente'>
+                            <div className='tituloCardPaciente'>
+                                <h2>Mateo Bernabé Lohezic</h2>
+                                <h3>Peso actual: 300 kg</h3>
+                            </div>
+                            <div className='turnoCardPaciente'>
+                                <img src={turnoIcon} alt="Proximo turno" />
+                                <div>
+                                    <h2>Proximo turno</ h2>
+                                    <h3>Martes 13 de agoto</h3>
+                                </div>
+                            </div>
+                            <div className='verCardPaciente'>
+                                <a href="/">
+                                    <button type="button">Ver planilla</button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                    <article className='cardPaciente'>
+                        <div className='imagenCardPaciente'>
+                            <img src={pacienteDefecto} alt="Paciente" />
+                        </div>
+                        <div className='detallesCardPaciente'>
+                            <div className='tituloCardPaciente'>
+                                <h2>Mateo Bernabé Lohezic</h2>
+                                <h3>Peso actual: 300 kg</h3>
+                            </div>
+                            <div className='turnoCardPaciente'>
+                                <img src={turnoIcon} alt="Proximo turno" />
+                                <div>
+                                    <h2>Proximo turno</ h2>
+                                    <h3>Martes 13 de agoto</h3>
+                                </div>
+                            </div>
+                            <div className='verCardPaciente'>
+                                <a href="/">
+                                    <button type="button">Ver planilla</button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                    <article className='cardPaciente'>
+                        <div className='imagenCardPaciente'>
+                            <img src={pacienteDefecto} alt="Paciente" />
+                        </div>
+                        <div className='detallesCardPaciente'>
+                            <div className='tituloCardPaciente'>
+                                <h2>Mateo Bernabé Lohezic</h2>
+                                <h3>Peso actual: 300 kg</h3>
+                            </div>
+                            <div className='turnoCardPaciente'>
+                                <img src={turnoIcon} alt="Proximo turno" />
+                                <div>
+                                    <h2>Proximo turno</ h2>
+                                    <h3>Martes 13 de agoto</h3>
+                                </div>
+                            </div>
+                            <div className='verCardPaciente'>
+                                <a href="/">
+                                    <button type="button">Ver planilla</button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                </section>
             </div>
         </main>
     </>
